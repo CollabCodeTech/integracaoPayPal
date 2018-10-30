78c5e8208d40ecc26afb5f8bc0579dc4f9e46f4e
Marco Bruno <marco.bruno.br@gmail.com>
2018-10-30 02:03:09 +0000

    Usando o verbo correto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 
 const { HOST, PORT } = process.env;
 
-server.get("/paypal", PayPalController.request);
+server.post("/paypal", PayPalController.request);
 
 server.listen(PORT, () => {
   console.log(`Servidor escutando em http://${HOST}:${PORT}`);
