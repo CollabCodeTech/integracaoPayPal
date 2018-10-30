d4d1a82faeab41c4ee7a7622d0c732ae8457ca55
Welington da Silva <thebylito@gmail.com>
2018-10-29 21:54:27 -0400

    url inicial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const { PORT } = process.env;
 
-server.get('/', (req, res) => {
+server.get('/home', (req, res) => {
   res.send('Olá');
 });
 
